e980911cc2c9ad74a82033b7e4e2651ea9345719
theharishpuri <theharishpuri@gmail.com>
2026-02-05 16:05:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
+// Get elements
+const searchBtn = document.getElementById("searchBtn");
+const cityInput = document.getElementById("cityInput");
+
+const cityName = document.getElementById("city");
+const temp = document.getElementById("temp");
+const desc = document.getElementById("desc");
+const humidity = document.getElementById("humidity");
+
+
+// When button clicked
+searchBtn.addEventListener("click", () => {
+    const city = cityInput.value.trim();
+
+    if (city === "") {
+        alert("Please enter a city name");
+        return;
+    }
+
+    getWeather(city);
+});
+
+
+// Function to fetch weather
+async function getWeather(city) {
+    const apiKey = "YOUR_API_KEY_HERE"; // we will add this next step
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        // Update UI
+        cityName.innerText = data.name;
+        temp.innerText = `Temperature: ${data.main.temp} °C`;
+        desc.innerText = `Condition: ${data.weather[0].description}`;
+        humidity.innerText = `Humidity: ${data.main.humidity}%`;
+
+    } catch (error) {
+        alert("City not found!");
+        console.log(error);
+    }
+}
 
