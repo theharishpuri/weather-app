78eb0603cd556de79baec4ca90bde348478ef283
theharishpuri <theharishpuri@gmail.com>
2026-02-06 16:03:22 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,23 @@
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open("weather-app").then((cache) => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/style.css",
+        "/script.js",
+        "/manifest.json"
+      ]);
+    })
+  );
+});
+
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
 
